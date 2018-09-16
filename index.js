d6eeb5fcc20a2ec22cfc7e4286a01acac739463d
matthewvolk <volkmattj@gmail.com>
2018-09-16 11:22:00 -0700

    add Google Authentication route handler

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+// Init Google OAuth 2.0 Strategy
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }), (accessToken) => {
+    console.log(accessToken);
+  }
+);
+
+// Google Authentication Route Handler
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
 
 // Route Handlers
 app.get('/', (req, res) => {
