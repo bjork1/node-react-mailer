45b16424f201001eacc3deb5996bc456afe1020f
matthewvolk <volkmattj@gmail.com>
2018-09-16 18:26:09 -0700

    add logout route to kill id of current user cookie

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,7 +14,12 @@ module.exports = (app) => {
     passport.authenticate('google')
   );
 
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  })
+
   app.get('/api/current_user', (req, res) => {
-    console.log(req.user);
+    res.send(req.user);
   })
 }
