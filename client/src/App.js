c26de46203065a6e1e1d06a9c8fc70c3b0dd6dd6
matthewvolk <volkmattj@gmail.com>
2018-09-20 20:35:41 -0700

    test changes to react proxy

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hi! I'm Emily.</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, click <a href="/auth/google">here</a> to log in with Google.
         </p>
       </div>
     );
