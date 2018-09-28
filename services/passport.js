dd44c9668d118f70fe6d0149cc5a3d3fa3a4761a
matthewvolk <volkmattj@gmail.com>
2018-09-27 19:50:00 -0700

    refactor MongoDB passport user save to use async/await

--- services/passport.js
+++ services/passport.js
@@ -24,18 +24,15 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
   }, 
-  (accessToken, refreshToken, profile, authComplete) => {
-    User.findOne({ googleID: profile.id })
-    .then((existingUser) => {
-      if (existingUser) {
-        // We already have a record with the given profile.id
-        authComplete(null, existingUser); // null: no error occurred 
-      } else {
-        // Mongoose does not persist this record unless save() is called with it.
-        new User({ googleID: profile.id })
-        .save()
-        .then(user => authComplete(null, user));
-      }
-    })
+  async (accessToken, refreshToken, profile, authComplete) => {
+    const existingUser = await User.findOne({ googleID: profile.id })
+    if (existingUser) {
+      // We already have a record with the given profile.id
+      authComplete(null, existingUser); // null: no error occurred 
+    } else {
+      // Mongoose does not persist this record unless save() is called with it.
+      const user = await new User({ googleID: profile.id }).save()
+      authComplete(null, user);
+    }
   })
 );
