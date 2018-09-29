f37eb0523dc632af2f09ef02808a3a94df08e241
matthewvolk <volkmattj@gmail.com>
2018-09-28 20:28:27 -0700

    create and combine authReducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render();
+import App from './components/App';
+import reducers from './reducers';
+
+// Create redux store
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  /**
+   * The <Provider /> tag is a react component that knows how to read changes
+   * from our redux store, so that every time the redux store gets some new 
+   * state produced inside of it, the <Provider /> will inform all of its children 
+   * components that some new state is available, and will update all of 
+   * those components with the new state. It's the glue that holds React and 
+   * Redux together.
+   */
+  <Provider store={store}><App /></Provider>, 
+  document.querySelector('#root')
+);
