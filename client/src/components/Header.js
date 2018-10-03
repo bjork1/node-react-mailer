071b788000c0b3ff3aa286953acf0b8ed4cd52f5
matthewvolk <volkmattj@gmail.com>
2018-10-02 20:34:06 -0700

    conditionally render header login/logout with redux connect

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false: 
+        return <a href="/auth/google">Login with Google</a>;
+      default:
+        return <a href="/api/logout">Logout</a>;
+    }
+  }
+
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-white">
         <div className="container">
           <a className="navbar-brand" href="/">Emily</a>
-          <a href="/auth/google" role="button">Login with Google</a>
+          {this.renderContent()}
         </div>
       </nav>
     )
   }
+};
+
+function mapStateToProps({ auth }) {
+  return { auth };
 }
 
-export default Header;
+export default connect(mapStateToProps)(Header);
