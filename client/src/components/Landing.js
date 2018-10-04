7974c4289f2ad686652c6aa34e17804a84dbbe52
matthewvolk <volkmattj@gmail.com>
2018-10-03 22:03:32 -0700

    change landing styles

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Landing = () => {
   return (
     <div className="container my-5">
-      <h1 className="text-center">Hi, I'm Emily.</h1>
-      <h5 className="text-center">I help you collect feedback from your users.</h5>
+      <h1 className="text-center display-4">Hi, I'm Emily.</h1>
+      <h4 className="text-center text-muted">I help you collect feedback from your users.</h4>
     </div>
   );
 }
