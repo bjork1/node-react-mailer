fd81900f3ecf0ec3397c019eb7fd84b7089a4040
matthewvolk <volkmattj@gmail.com>
2018-10-06 13:58:59 -0700

    add credits to user model on stripe successful charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,6 +3,10 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post('/api/stripe', async (req, res) => {
+    if (!req.user) {
+      return res.status(401).send({ error: 'You must be logged in to make this request' })
+    }
+    
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
@@ -10,6 +14,10 @@ module.exports = app => {
       source: req.body.id
     });
 
-    console.log(charge);
+    // Passport middleware gives us access to the current user logged in making this request
+    req.user.credits += 5000;
+    const user = await req.user.save();
+
+    res.send(user);
   });
 };
