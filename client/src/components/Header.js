341e738e69e4ab47643149f03ddbfa137ccb52bb
matthewvolk <volkmattj@gmail.com>
2018-10-06 15:30:49 -0700

    button color change

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,12 +9,12 @@ class Header extends Component {
       case null:
         return;
       case false: 
-        return <li><a className="btn btn-sm btn-warning mx-1" href="/auth/google">Login with Google</a></li>;
+        return <li><a className="btn btn-sm btn-warning mx-1" style={{ backgroundColor: '#ffb982', borderColor: '#ffb982' }} href="/auth/google">Login with Google</a></li>;
       default:
         return [
           <li key="3" className="my-1 mx-1">Credits: {this.props.auth.credits}</li>,
           <li key="1"><StripePayments /></li>,
-          <li key="2"><a className="btn btn-sm btn-warning mx-1" href="/api/logout">Logout</a></li>
+          <li key="2"><a className="btn btn-sm btn-warning mx-1" style={{ backgroundColor: '#ffb982', borderColor: '#ffb982' }} href="/api/logout">Logout</a></li>
         ];
     }
   }
@@ -23,7 +23,10 @@ class Header extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-white">
         <div className="container">
-          <Link className="navbar-brand" to={ this.props.auth ? '/surveys' : '/' }>MailShrimp</Link>
+          <Link className="navbar-brand mr-0" to={ this.props.auth ? '/surveys' : '/' }>
+            <img src="https://i.imgur.com/d4StpWT.png" width="30" height="30" className="d-inline-block align-top mr-2" alt="Shrimp Logo"></img>
+            MailShrimp
+          </Link>
 
           <ul className="navbar-nav ml-auto" style={{ flexDirection: 'row' }}>
             {this.renderContent()}
