1b2fa6fa1ca493e89dd566fdd98a7b2395c5031e
matthewvolk <volkmattj@gmail.com>
2018-10-06 13:50:59 -0700

    add credits property to user model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userShema = new Schema({
-  googleID: String
+  googleID: String,
+  credits: {
+    type: Number,
+    default: 0
+  }
 });
 
 mongoose.model('users', userShema);
