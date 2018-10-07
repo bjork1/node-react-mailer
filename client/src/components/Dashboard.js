fe9ea242c3685f681e5bc507684fe01c1e82e3ee
matthewvolk <volkmattj@gmail.com>
2018-10-07 15:32:59 -0700

    add Dashboard component

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Dashbaord = () => {
+  return (
+    <div className="container">
+      <h3>Dashboard</h3>
+      <p><strong>To test this application, use test credit card credentials:</strong></p>
+      <ul>
+        <li><strong>Credit Card Number:</strong> 4242 4242 4242 4242</li>
+        <li><strong>Test Expiration Date:</strong> 12/22</li>
+        <li><strong>Test CCV Number:</strong> 222</li>
+      </ul>
+    </div>
+  )
+}
