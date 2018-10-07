d0f01d09665d565a31ffb1465b80903048f8a7a2
matthewvolk <volkmattj@gmail.com>
2018-10-07 14:36:49 -0700

    test with axios again

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,16 @@
 module.exports = survey => {
-  return `<div>${survey.body}</div>`;
+  return `
+    <html>
+      <body>
+        <div style="text-align: center">
+          <h3>Hi! It's been a while since you purchased our {product}!</h3>
+          <p>${survey.body}</p>
+          <div>
+            <a href="http://localhost:3000">Yes</a>
+            <a href="http://localhost:3000">No</a>
+          </div>
+        </div>
+      </body>
+    </html>
+  `;
 };
