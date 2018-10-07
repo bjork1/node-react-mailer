c8581f691deb2722decd763c936580f9d871ce7d
matthewvolk <volkmattj@gmail.com>
2018-10-07 14:22:46 -0700

    remove axios for testing

--- client/src/index.js
+++ client/src/index.js
@@ -9,10 +9,6 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-// Test Mailer.js
-import axios from 'axios';
-window.axios = axios;
-
 // Create redux store
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
