7620696fdee5c51985acb78b42da781b99ca56c0
matthewvolk <volkmattj@gmail.com>
2018-10-06 22:14:26 -0700

    handle survey post data

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,22 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    // Destructure incoming request body for the following data:
+    const { title, subject, body, recipients } = req.body;
 
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      // Convert array of strings into a list of objects
+      recipients: recipients.split(',').map(email =>  ({ email })),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    })
   });
 };
