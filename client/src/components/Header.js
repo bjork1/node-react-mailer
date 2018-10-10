4108902d7426fd6f45858532c5d7b777cb44e9a4
matthewvolk <volkmattj@gmail.com>
2018-10-09 18:00:03 -0700

    fix mobile menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,12 +9,12 @@ class Header extends Component {
       case null:
         return;
       case false: 
-        return <li><a className="btn btn-sm btn-warning mx-1" style={{ backgroundColor: '#ffb982', borderColor: '#ffb982' }} href="/auth/google">Login with Google</a></li>;
+        return <li className="nav-item"><a className="btn btn-sm btn-warning mx-1" style={{ backgroundColor: '#ffb982', borderColor: '#ffb982' }} href="/auth/google">Login with Google</a></li>;
       default:
         return [
-          <li key="3" className="my-1 mx-1">Credits: {this.props.auth.credits}</li>,
-          <li key="1"><StripePayments /></li>,
-          <li key="2"><a className="btn btn-sm btn-warning mx-1" style={{ backgroundColor: '#ffb982', borderColor: '#ffb982' }} href="/api/logout">Logout</a></li>
+          <li className="nav-item" key="3" className="my-1 mx-1">Credits: {this.props.auth.credits}</li>,
+          <li className="nav-item" key="1"><StripePayments /></li>,
+          <li className="nav-item" key="2"><a className="btn btn-sm btn-warning mx-1" style={{ backgroundColor: '#ffb982', borderColor: '#ffb982' }} href="/api/logout">Logout</a></li>
         ];
     }
   }
@@ -23,15 +23,21 @@ class Header extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-white">
         <div className="container">
-          <Link className="navbar-brand mr-0" to={ this.props.auth ? '/surveys' : '/' }>
+
+          <Link className="navbar-brand" to={ this.props.auth ? '/surveys' : '/' }>
             <img src="https://i.imgur.com/d4StpWT.png" width="30" height="30" className="d-inline-block align-top mr-2" alt="Shrimp Logo"></img>
             MailShrimp
           </Link>
 
-          <ul className="navbar-nav ml-auto" style={{ flexDirection: 'row' }}>
-            {this.renderContent()}
-          </ul>
-          
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto" style={{ flexDirection: 'row' }}>
+              {this.renderContent()}
+            </ul>
+          </div>
         </div>
       </nav>
     )
