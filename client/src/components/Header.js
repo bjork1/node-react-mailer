305d266562a981972d3cc639c0d9c60dcd8df5f6
matthewvolk <volkmattj@gmail.com>
2018-10-10 22:23:33 -0700

    fix navbar toggle border

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -29,12 +29,12 @@ class Header extends Component {
             MailShrimp
           </Link>
 
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" style={{ borderColor: 'white' }} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto" style={{ flexDirection: 'row' /* For Mobile Right-Align: justify-content: flex-end; */ }}>
+            <ul className="navbar-nav ml-auto" style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
               {this.renderContent()}
             </ul>
           </div>
