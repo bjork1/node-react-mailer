80babcaed5345b38a659cc8406ef6a0595fb0702
matthewvolk <volkmattj@gmail.com>
2018-10-10 21:57:58 -0700

    add content to landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,8 +3,22 @@ import React from 'react';
 const Landing = () => {
   return (
     <div className="container my-5">
-      <h1 className="text-center">Welcome to MailShrimp</h1>
-      <h5 className="text-center text-muted">We help you collect feedback from your users via email.</h5>
+      <div className="jumbotron mb-0 bg-white text-center">
+        <h1 className="display-4">Welcome to MailShrimp</h1>
+        <p className="lead">The Simplest Way to Send a Ton of Pointless Emails</p>
+      </div>
+      <div className="jumbotron mb-0 py-4 bg-white text-center">
+        <h1 className="display-4">Step 1.</h1>
+        <p className="lead">Log In with Google</p>
+      </div>
+      <div className="jumbotron mb-0 py-4 bg-white text-center">
+        <h1 className="display-4">Step 2.</h1>
+        <p className="lead">Add Credits to your Account</p>
+      </div>
+      <div className="jumbotron mb-0 py-4 bg-white text-center">
+        <h1 className="display-4">Step 3.</h1>
+        <p className="lead">Create a new Survey!</p>
+      </div>
     </div>
   );
 }
